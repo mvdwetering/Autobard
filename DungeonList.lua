12365c7dab11fd06aa6f19efeb952307fe3cebc2
Michel van de Wetering <git@michelvandewetering.nl>
2014-05-02 23:53:34 +0200

    Added some missing dungeons and updated rep --> dungeon mapping. Mostly for MISTS factions that seem to work in all dungeons.

--- DungeonList.lua
+++ DungeonList.lua
@@ -12,101 +12,104 @@ local dl = {}
 
 -- *** Classic dungeons ***
 
-dl[680] = { normal = ATBD_REPGROUP_CITY } -- RagefireChasm
-dl[686] = { normal = ATBD_REPGROUP_CITY } -- Zul'farrak
-dl[687] = { normal = ATBD_REPGROUP_CITY } -- Sunken Temple
-dl[688] = { normal = ATBD_REPGROUP_CITY } -- Black Fathom Deeps
-dl[690] = { normal = ATBD_REPGROUP_CITY } -- Stockades
-dl[691] = { normal = ATBD_REPGROUP_CITY } -- Gnomeregan
-dl[692] = { normal = ATBD_REPGROUP_CITY } -- Uldaman
-dl[699] = { normal = ATBD_REPGROUP_CITY } -- DireMaul
-dl[704] = { normal = ATBD_REPGROUP_CITY } -- Blackrock Depths
-dl[721] = { normal = ATBD_REPGROUP_CITY } -- BlackrockSpire
-dl[749] = { normal = ATBD_REPGROUP_CITY } -- WailingCaverns
-dl[750] = { normal = ATBD_REPGROUP_CITY } -- Maraudon
-dl[760] = { normal = ATBD_REPGROUP_CITY } -- Razorfen Downs
-dl[761] = { normal = ATBD_REPGROUP_CITY } -- Razorfen Kraul
-dl[762] = { normal = ATBD_REPGROUP_CITY } -- Scarlet Monastry
-dl[763] = { normal = ATBD_REPGROUP_CITY } -- Scholomance
-dl[765] = { normal = ATBD_REPGROUP_CITY } -- Stratholme
+dl[680] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- RagefireChasm
+dl[686] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Zul'farrak
+dl[687] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Sunken Temple
+dl[688] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Black Fathom Deeps
+dl[690] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Stockades
+dl[691] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Gnomeregan
+dl[692] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Uldaman
+dl[699] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- DireMaul
+dl[704] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Blackrock Depths
+dl[721] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- BlackrockSpire
+dl[749] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- WailingCaverns
+dl[750] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Maraudon
+dl[760] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Razorfen Downs
+dl[761] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Razorfen Kraul
+dl[762] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Scarlet Monastry
+dl[763] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Scholomance
+dl[765] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Stratholme
 
 
 -- *** BC dungeons ***
 -- These provide no rep through tabards (yet?)
 
-dl[710] = { } -- The Shattered Halls
-dl[797] = { } -- Hellfire Ramparts
-dl[725] = { } -- The Blood Furnace
+dl[710] = { normal = ATBD_REPGROUP_MISTS } -- The Shattered Halls
+dl[797] = { normal = ATBD_REPGROUP_MISTS } -- Hellfire Ramparts
+dl[725] = { normal = ATBD_REPGROUP_MISTS } -- The Blood Furnace
 
-dl[726] = { } -- The Underbog
-dl[727] = { } -- The Steamvault
-dl[728] = { } -- The Slave Pens
+dl[726] = { normal = ATBD_REPGROUP_MISTS } -- The Underbog
+dl[727] = { normal = ATBD_REPGROUP_MISTS } -- The Steamvault
+dl[728] = { normal = ATBD_REPGROUP_MISTS } -- The Slave Pens
 
-dl[724] = { } -- Shadow Labyrinth
-dl[732] = { } -- Mana-Tombs
-dl[722] = { } -- Auchenai Crypts
-dl[723] = { } -- Sethekk Halls
+dl[724] = { normal = ATBD_REPGROUP_MISTS } -- Shadow Labyrinth
+dl[732] = { normal = ATBD_REPGROUP_MISTS } -- Mana-Tombs
+dl[722] = { normal = ATBD_REPGROUP_MISTS } -- Auchenai Crypts
+dl[723] = { normal = ATBD_REPGROUP_MISTS } -- Sethekk Halls
 
-dl[729] = { } -- The Botanica
-dl[730] = { } -- The Mechanar
-dl[731] = { } -- The Arcatraz
+dl[729] = { normal = ATBD_REPGROUP_MISTS } -- The Botanica
+dl[730] = { normal = ATBD_REPGROUP_MISTS } -- The Mechanar
+dl[731] = { normal = ATBD_REPGROUP_MISTS } -- The Arcatraz
 
-dl[734] = { } -- CoT: Old Hillsbrad
-dl[733] = { } -- CoT: The Black Morass
+dl[734] = { normal = ATBD_REPGROUP_MISTS } -- CoT: Old Hillsbrad
+dl[733] = { normal = ATBD_REPGROUP_MISTS } -- CoT: The Black Morass
 
-dl[798] = { } -- Magister's Terrace
+dl[798] = { normal = ATBD_REPGROUP_MISTS } -- Magister's Terrace
 
 
 -- *** WotLK Dungeons ***
 
-dl[523] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- Utgarde Keep
-dl[520] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- The Nexus
-dl[533] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- Azjol-Nerub: Azjol-Nerub
-dl[522] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- Ahn'kahet: The Old Kingdom
-dl[534] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- Drak'Tharon Keep
-dl[536] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- The Violet Hold
-dl[530] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- Gundrak
-dl[526] = { normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK }  -- Halls of Stone
-dl[528] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Oculus
-dl[525] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Halls of Lightning
-dl[524] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Utgarde Pinnacle
-dl[521] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Culling of Stratholme
-dl[542] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Trial of the Champion
-dl[601] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Forge of Souls
-dl[602] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Pit of Saron
-dl[603] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK }  -- Halls of Reflection
+dl[523] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- Utgarde Keep
+dl[520] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- The Nexus
+dl[533] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- Azjol-Nerub: Azjol-Nerub
+dl[522] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- Ahn'kahet: The Old Kingdom
+dl[534] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- Drak'Tharon Keep
+dl[536] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- The Violet Hold
+dl[530] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- Gundrak
+dl[526] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK }  -- Halls of Stone
+dl[528] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Oculus
+dl[525] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Halls of Lightning
+dl[524] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Utgarde Pinnacle
+dl[521] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Culling of Stratholme
+dl[542] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Trial of the Champion
+dl[601] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Forge of Souls
+dl[602] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Pit of Saron
+dl[603] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS }          -- Halls of Reflection
 
 -- *** Cataclysm dungeons ***
 
-dl[756] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }   -- The Deadmines
-dl[764] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY, heroic = ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }   -- Shadowfang Keep
-dl[753] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK, heroic = ATBD_REPGROUP_CATA }  -- Blackrock Caverns
-dl[767] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK, heroic = ATBD_REPGROUP_CATA }  -- Throne of Tides
-dl[768] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK, heroic = ATBD_REPGROUP_CATA }  -- Stonecore
-dl[769] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK, heroic = ATBD_REPGROUP_CATA }  -- Vortex Pinnacle
-dl[759] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- GrimBatol
-dl[759] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- Halls of Origination
-dl[747] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- Lost City of Tol'vir
+dl[756] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }   -- The Deadmines
+dl[764] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }   -- Shadowfang Keep
+dl[753] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_CATA }  -- Blackrock Caverns
+dl[767] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_CATA }  -- Throne of Tides
+dl[768] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_CATA }  -- Stonecore
+dl[769] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS, heroic = ATBD_REPGROUP_CATA }  -- Vortex Pinnacle
+dl[759] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- GrimBatol
+dl[759] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- Halls of Origination
+dl[747] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- Lost City of Tol'vir
 
 -- *** Patch 4.1 dungeons ***
-dl[793] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- Zul'Gurub
-dl[781] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- Zul'Aman
+dl[793] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- Zul'Gurub
+dl[781] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- Zul'Aman
 
 -- *** Patch 4.3 dungeons ***
-dl[820] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- End Time
-dl[816] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- Well of Eternity
-dl[819] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA }          -- Hour of Twilight
+dl[820] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- End Time
+dl[816] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- Well of Eternity
+dl[819] = { xpack = ATBD_EXP_CATA, normal = ATBD_REPGROUP_CITY  + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS }          -- Hour of Twilight
 
 
 -- *** Patch 5.0.4 dungeons ***
-dl[871] = { normal = ATBD_REPGROUP_CITY } -- Scarlett Halls
+dl[871] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Scarlett Halls
+dl[874] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Scarlet Monastery
+dl[898] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_MISTS } -- Scholomance
 
 -- *** Mists dungeons *** THE REPS ARE GUESSES FOR NOW
-dl[875] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS } -- Gate of the Setting Sun MoP
-dl[885] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS } -- Mogu'Shan Palace MoP
-dl[877] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS } -- Shado-pan Monastery MoP
-dl[887] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS } -- Siege of Niuzao Temple MoP
-dl[876] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS } -- Stormstout Brewery MoP
-dl[867] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_CATA + ATBD_REPGROUP_MISTS } -- Temple of the Jade Serpent 
+dl[875] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS } -- Gate of the Setting Sun MoP
+dl[885] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS } -- Mogu'Shan Palace MoP
+dl[877] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS } -- Shado-pan Monastery MoP
+dl[887] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS } -- Siege of Niuzao Temple MoP
+dl[876] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS } -- Stormstout Brewery MoP
+dl[867] = { normal = ATBD_REPGROUP_CITY + ATBD_REPGROUP_WOTLK + ATBD_REPGROUP_MISTS } -- Temple of the Jade Serpent 
+
 
 ATBD.dungeons = dl
